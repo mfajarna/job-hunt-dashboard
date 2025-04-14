656a20f6163a2a6a4f3adb4709e325388ca63493
muhammad fajar <invasionfajar@gmail.com>
2025-04-15 02:50:16 +0700

    update base-service

--- src/lib/base-service.ts
+++ src/lib/base-service.ts
@@ -21,10 +21,11 @@ export default function baseService({
   signal,
   timeout,
 }: BaseServiceType) {
-  // Enable when using development env
-  const url: string = process.env.NEXT_PUBLIC_BASE_URL + path;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const url: string = (baseUrl ?? '') + path;
+  // const url = path;
 
-  // const url: string = path;
+  console.log('url', url);
 
   return instance
     .request({
@@ -36,6 +37,8 @@ export default function baseService({
       timeout,
     })
     .then((res) => {
+      // console.log('Data Response', res);
+
       return res.data;
     });
 }
